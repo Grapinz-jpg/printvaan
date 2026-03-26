2d6d8b93b80bb66eb313c17ae3384afb0a5f9484
Grapinz-jpg <your-email-used-in-grapinz>
2026-03-26 16:20:57 +0530

    supabase.js correction

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://cjrkryplyxkvjdfywyqg.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqcmtyeXBseXhrdmpkZnl3eXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MDEwMjYsImV4cCI6MjA5MDA3NzAyNn0.Qwa75a7iEAvOLzfFT90MxUtj1Aldrc2dggaqUYMHBPw";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-console.log("Supabase URL:", supabaseUrl);
