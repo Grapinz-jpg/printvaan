a335f5ff6cc87c68565d0298147dbf126f1d549f
Grapinz-jpg <your-email-used-in-grapinz>
2026-03-26 15:32:18 +0530

    .env.local file links corrected

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://fkrkqifldssamfzobnko.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrcmtxaWZsZHNzYW1mem9ibmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMjg1MjksImV4cCI6MjA4OTgwNDUyOX0.QFrMQ8UKHynRvl18-sVlg-H47GbwTJbf-bpvCORX9kI";
+const supabaseUrl = "https://cjrkryplyxkvjdfywyqg.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqcmtyeXBseXhrdmpkZnl3eXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MDEwMjYsImV4cCI6MjA5MDA3NzAyNn0.Qwa75a7iEAvOLzfFT90MxUtj1Aldrc2dggaqUYMHBPw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("Supabase URL:", supabaseUrl);
